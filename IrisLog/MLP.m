c4d64cd1f0d1856e622f2bd3c32e33f4aba480dd
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-05-23 17:13:21 -0300

    MLP implementada e testada com hold-out, 10-fold e leave-one-out

--- IrisLog/MLP.m
+++ IrisLog/MLP.m
@@ -11,13 +11,6 @@ X = iris_log(:,1:4)';
 Y = iris_log(:,5:7)';
 
 %Criar a rede MLP
-%Entrada -> X
-%Saída -> Y
-%[10 3] -> rede neural com duas camadas ocultas, a primeira com 10
-%neurônios e a segunda com 3 neurônios
-%logsig -> função de ativação logistica
-%traingdx -> função de treinamento, chamada de Backpropagation de gradiente
-%decrescente com momentum e taxa adaptative
 net = newff(X,Y,[10 3], {'logsig' 'logsig'}, 'traingdx');
 
 
@@ -51,14 +44,15 @@ str = ['Acuracia obtida com hold-out: ' num2str(100*acertos/length(Xteste)) ' %'
 disp(str); %exibe a Acuracia obtida
 
 %----10-fold----
+z = length(X)/10;
 for i=1:10
-    XTreinamento = X; 
-    YTreinamento = Y;
-    for j=15:-1:1
-        XTeste(:,j) = X(:,(i-1)*15 + j); %amostras para testar o modelo
-        XTreinamento(:,(i-1)*15 + j) = []; %ficam so as amostras que vão treinar o modelo
-        YTeste(:,j) = Y(:,(i-1)*15 + j); %classes para verificar acuracia do modelo
-        YTreinamento(:,(i-1)*15 + j) = []; %ficam so as classes que vão treinar o modelo
+    Xtreinamento = X; 
+    Ytreinamento = Y;
+    for j=z:-1:1
+        Xteste(:,j) = X(:,(i-1)*15 + j); %amostras para testar o modelo
+        Xtreinamento(:,(i-1)*15 + j) = []; %ficam so as amostras que vão treinar o modelo
+        Yteste(:,j) = Y(:,(i-1)*15 + j); %classes para verificar acuracia do modelo
+        Ytreinamento(:,(i-1)*15 + j) = []; %ficam so as classes que vão treinar o modelo
     end
     %treinando a rede
     net = train(net,Xtreino,Ytreino);%função para treinar a rede com as amostras treino
@@ -73,7 +67,30 @@ for i=1:10
             acertos=acertos+1;
         end
     end
-    str = ['Acuracia obtida com 10-fold: ' num2str(100*acertos/length(Xteste)) ' %'];
-    disp(str); %exibe a Acuracia obtida
 end
+str = ['Acuracia media obtida pelo 10-fold: ' num2str(100*acertos/length(Xteste)) ' %'];
+disp(str); %exibe a Acuracia obtida
 
+%----leave-one-out----
+acertos=0;
+for i=1:150
+    Xtreino = X; 
+    Ytreino = Y;
+    Xteste = X(:,i); %amostra para testar o modelo
+    Xtreino(:,i) = []; %ficam so as amostras que vão treinar o modelo
+    Yteste = Y(:,i); %classe para verificar acuracia do modelo
+    Ytreino(:,i) = []; %ficam so as classes que vão treinar o modelo
+  
+    %treinando a rede
+    net = train(net,Xtreino,Ytreino);%função para treinar a rede com as amostras treino
+    
+    %Testando a rede
+    Yout = round(net(Xteste));%testar a rede com as amostras teste
+    
+    %verifica acuracia do metodo
+    if Yteste == Yout;
+        acertos=acertos+1;
+    end
+end
+str = ['Acuracia obtida pelo leave-one-out: ' num2str(100*acertos/length(X)) ' %'];
+disp(str); %exibe a Acuracia obtida
